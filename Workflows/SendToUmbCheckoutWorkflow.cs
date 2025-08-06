c6dc8ee1ab33b997a8ca2f81ca5ceda1a9b24a3a
Aaron Sadler <aaron@umbhost.net>
2025-08-06 18:01:32 +0100

    Removed net 8

--- Workflows/SendToUmbCheckoutWorkflow.cs
+++ Workflows/SendToUmbCheckoutWorkflow.cs
@@ -32,8 +32,6 @@ namespace UmbCheckout.Stripe.Forms.Workflows
 
         [Setting("Email alias", Description = "The alias of the field used to capture the email address", DisplayOrder = 70, View = "TextField")]
         public string EmailAlias { get; set; } = "emailAddress";
-
-#if NET8_0_OR_GREATER
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public override async Task<WorkflowExecutionStatus> ExecuteAsync(WorkflowExecutionContext context)
 #pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
@@ -74,46 +72,6 @@ namespace UmbCheckout.Stripe.Forms.Workflows
                 return WorkflowExecutionStatus.Failed;
             }
         }
-#else
-        public override WorkflowExecutionStatus Execute(WorkflowExecutionContext context)
-        {
-            try
-            {
-                if (context == null)
-                {
-                    _logger.LogInformation("WorkflowExecutionContext Context is null");
-                    _logger.LogCritical("WorkflowExecutionContext Context is null");
-                    throw new Exception("Umbraco Context is missing... Why?");
-                }
-
-                var basket = _basketService.Get().Result;
-                basket.CustomerReferenceId = $"{context.Form.Id}@{context.Record.UniqueId}";
-
-                var emailAddress = context.Record.GetRecordFieldByAlias(EmailAlias);
-                if (emailAddress != null && emailAddress.HasValue())
-                {
-                    var emailValue = context.Record.GetRecordFieldByAlias(EmailAlias)?.ValuesAsString();
-                    if (!string.IsNullOrEmpty(emailValue))
-                    {
-                        basket.Customer = new Customer
-                        {
-                            EmailAddress = emailValue
-                        };
-                    }
-                }
-                var stripeSession = _sessionService.CreateSessionAsync(basket).Result;
-                if (_httpContextAccessor.HttpContext != null)
-                    _httpContextAccessor.HttpContext.Items[Constants.ItemKeys.RedirectAfterFormSubmitUrl] =
-                        stripeSession.Url;
-                return WorkflowExecutionStatus.Completed;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogCritical(ex, ex.Message);
-                return WorkflowExecutionStatus.Failed;
-            }
-        }
-#endif
 
         public override List<Exception> ValidateSettings()
         {
